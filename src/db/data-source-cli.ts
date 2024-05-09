d6813e67a21e06ac7c88a7b13e297fe9a6199d11
danielmedeiros52 <danielmedeiros52@gmail.com>
2024-05-08 23:37:08 -0300

    vercel deploy

--- src/db/data-source-cli.ts
+++ src/db/data-source-cli.ts
@@ -4,12 +4,12 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   synchronize: true,
   dropSchema: false,
   logging: true,
+  ssl: true,
   database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.finance.entity.{js,ts}'],
   migrations: [__dirname + '/migrations/*.{js,ts}'],

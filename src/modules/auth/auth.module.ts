69cff00d347d16316554a3c6b03b0512623625d5
Daniel Brito <dlmf@gft.com>
2024-05-13 09:30:02 -0300

    added access group

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,7 +6,6 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { EmailServiceModule } from '../email-service/email-service.module';
 import { WalletModule } from '../wallet/wallet.module';
-import { GroupModule } from './group/group.module';
 
 @Module({
   imports: [
@@ -23,7 +22,6 @@ import { GroupModule } from './group/group.module';
       inject: [ConfigService],
       global: true,
     }),
-    GroupModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
